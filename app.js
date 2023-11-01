738c92ea8ea5bf63f5ad560385e171bdd3176ad7
unknown <m.jahansoz1999@gmail.com>
2023-11-01 18:05:11 +0330

    version 1

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const createWallet = async () => {
     ledger_index: "validated",
   });
   console.log(response);
-
   await client.disconnect();
 };
 
@@ -24,38 +23,70 @@ const getBalance = async (address) => {
 };
 
 const sendXrp = async (secret, destinationAddress, amount) => {
-  const fee = 0.1;
   await client.connect();
+
+  const fee = 0.1;
+  const minimumBalance = 1;
   const wallet = xrpl.Wallet.fromSecret(secret);
-  const balance = await client.getBalances(wallet.address);
-  console.log(balance);
-  console.log(wallet);
+  const walletBalance = await client.getBalances(wallet.address);
+  const balance = walletBalance[0].value;
+  const transmissible_balance =
+    xrpl.xrpToDrops(balance) -
+    xrpl.xrpToDrops(fee) -
+    xrpl.xrpToDrops(minimumBalance);
+  // we can't decrease balance more than 1 xrp
+
+  const amount_xrp = xrpl.dropsToXrp(transmissible_balance);
+  if (amount_xrp < 1.5) {
+    console.log("not enought balance");
+    return null;
+  }
+
+  const memo =
+    wallet.classicAddress + Math.floor(100000000 + Math.random() * 900000000);
   const prepared = await client.autofill({
     TransactionType: "Payment",
     Account: wallet.address,
-    Amount: xrpl.xrpToDrops(5),
+    Amount: xrpl.xrpToDrops(amount_xrp),
     Destination: destinationAddress,
     Fee: xrpl.xrpToDrops(fee),
+    Memos: [
+      {
+        Memo: {
+          MemoData: Buffer.from(memo, "utf8").toString("hex").toUpperCase(),
+        },
+      },
+    ],
   });
-  const max_ledger = prepared.LastLedgerSequence;
+
+  // const max_ledger = prepared.LastLedgerSequence;
   console.log("Prepared transaction instructions:", prepared);
-  console.log("Transaction cost:", xrpl.dropsToXrp(prepared.Fee), "XRP");
-  console.log("Transaction expires after ledger:", max_ledger);
+  // console.log("Transaction cost:", xrpl.dropsToXrp(prepared.Fee), "XRP");
+  // console.log("Transaction expires after ledger:", max_ledger);
 
   const signed = wallet.sign(prepared);
-  console.log("Identifying hash:", signed.hash);
-  console.log("Signed blob:", signed.tx_blob);
+  console.log("Transaction Id", signed.hash);
+  // console.log("Signed blob:", signed.tx_blob);
 
   const tx = await client.submitAndWait(signed.tx_blob);
+  // const output = Buffer.from(tx.result.Memos[0].Memo.MemoData, "hex");
+  // console.log("test", output.toString());
   console.log(tx);
-  console.log("Transaction result:", tx.result.meta.TransactionResult);
-  console.log(
-    "Balance changes:",
-    JSON.stringify(xrpl.getBalanceChanges(tx.result.meta), null, 2)
-  );
+  console.log(`transaction result ${tx.result.meta.TransactionResult}`);
+  console.log(`Transaction amount deliver ${tx.result.meta.delivered_amount}`);
+
   await client.disconnect();
 };
 
-sendXrp("snqt5zDTrhb7z1KxAmPB9Pnjja535", "rJyx9wZCkRopX4WX4ZMmrN5KDkU4FeEgKu");
+const testClient = async function () {
+  await client.connect();
+  console.log(client.isConnected());
+  await client.disconnect();
+};
+
+// testClient();
+
+// sendXrp("snZBfy5ovwe48fgZAzePihkSjVEFF", "rUn4kU7UAej8d8ddHc1PrRHwEzd3G9KFo2");
+
 // getBalance("rUn4kU7UAej8d8ddHc1PrRHwEzd3G9KFo2");
 // createWallet();
